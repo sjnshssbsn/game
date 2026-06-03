3698b5a7010563aa7ef1639b50efcf9f69622d14
student <student@eduiteh.eu>
2026-06-03 09:45:56 +0300

    v12

--- Project g/Project g/Game1.cs
+++ Project g/Project g/Game1.cs
@@ -61,8 +61,7 @@ namespace Project_g
             _squareTexture = new Texture2D(GraphicsDevice, 1, 1);
             _squareTexture.SetData(new[] { Color.Beige });
 
-            Texture2D playerTexture = new Texture2D(GraphicsDevice, 1, 1);
-            playerTexture.SetData(new[] { Color.Beige });
+            Texture2D playerTexture = Content.Load<Texture2D>(:"images/Player");
             _player.LoadContent(playerTexture);
         }
 
