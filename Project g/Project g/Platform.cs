e861dc29a7a65018c33c558c00f750ed1c247c38
student <student@eduiteh.eu>
2026-06-09 12:27:08 +0300

    v12

---
+++ Project g/Project g/Platform.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_g
+{
+    public class Platform
+    {
+        private Rectangle _collider;
+    }
+}
