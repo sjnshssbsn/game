c191bca61f7bd5fdb95349b34b1452b667bda090
student <student@eduiteh.eu>
2026-06-10 09:50:21 +0300

    v12.2

--- Project g/Project g/Platform.cs
+++ Project g/Project g/Platform.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +12,10 @@ namespace Project_g
     public class Platform
     {
         private Rectangle _collider;
+        private Vector2 _position;
+        private Texture2D texture;
+        public Platform(Vector2 position, Vector2 size) {
+        _position = position;
+        }
     }
 }
